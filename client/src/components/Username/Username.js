dd518d84643f46806d58baf47a1cebe9a9a4bd7b
trieunguyentm <trieunguyen241102@gmail.com>
2023-10-07 01:39:50 +0700

    UI username and react hook form

--- client/src/components/Username/Username.js
+++ client/src/components/Username/Username.js
@@ -1,25 +1,48 @@
 import React from "react"
 import { Button, Divider, Paper } from "@mui/material"
+import { useForm } from "react-hook-form"
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
 import "./Username.css"
 
 const Username = () => {
+  // Khai báo useForm
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+    },
+    mode: "onChange",
+  })
+
   return (
     <div className="login-container">
-      <Paper elevation={10}>
-        <div className="login-title">Đăng nhập</div>
-        <form>
+      <Paper elevation={10} style={{ borderRadius: "20px", width: "400px" }}>
+        {/* Title Page */}
+        <div className="login-title">
+          <AccountCircleIcon style={{ fontSize: "50px" }} />
+        </div>
+        {/* Form Page */}
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
           <div className="login-form">
-            <label className="label-username" htmlFor="username">
-              Tên đăng nhập:
-            </label>
-
-            <input
-              className="username"
-              id="username"
-              name="username"
-              placeholder="Nhập tên đăng nhập "
-            />
-
+            {/* Input Username */}
+            <div className="login-form-username">
+              <input
+                {...register("username", {
+                  required: {
+                    value: true,
+                    message: "Vui lòng điền thông tin đăng nhập",
+                  },
+                })}
+                style={{ width: "180px" }}
+                className="username"
+                placeholder="Nhập tên đăng nhập "
+              />
+            </div>
+            <div className="error-username">{errors.username?.message}</div>
+            {/* Button Login */}
             <div className="btn-submit-username-container">
               <Button
                 type="submit"
@@ -32,14 +55,14 @@ const Username = () => {
           </div>
         </form>
         <Divider />
-
+        {/* If no account create link register */}
         <div className="no-account">
           <span>Nếu chưa có tài khoản - </span>
           <a href="/register" style={{ textDecoration: "underline" }}>
             Đăng ký tại đây
           </a>
         </div>
-
+        {/* If forgot password create link recovery */}
         <div className="forgot-password">
           <span>Quên mật khẩu - </span>
           <a href="/recovery" style={{ textDecoration: "underline" }}>
