26b283237c2bab4bbc9d5ceb6680bca9520b2ee2
trieunguyentm <trieunguyen241102@gmail.com>
2023-10-11 15:58:37 +0700

    restructure

--- server/router/route.js
+++ server/router/route.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import * as controller from "../controllers/appController.js"
-import { authUser } from "../middlewares/authUser.js"
+import * as middleware from "../middlewares/appMiddleware.js"
 
 const router = Router()
 
@@ -10,12 +10,12 @@ router.post("/register", controller.register)
 router.post("/registerMail")
 router.post("/authenticate")
 /** GET Methods */
-router.get("/user/:username", authUser, controller.getUser)
+router.get("/user/:username", middleware.authUser, controller.getUser)
 router.get("/generateOTP")
 router.get("/verifyOTP")
 router.get("/createResetSession")
 /** PUT Methods */
-router.put("/updateuser/:username", authUser, controller.updateUser)
+router.put("/updateuser/:username", middleware.authUser, controller.updateUser)
 router.put("/resetPassword")
 
 export default router
